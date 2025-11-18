a741b555bb2b38aa3f7e65c883010df641f21ebd
Radu Vlad <radu.a.vlad@gmail.com>
2025-11-18 00:45:11 -0800

    Add logo to header

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  // Set directories to pass through to the _site folder
+  eleventyConfig.addPassthroughCopy("_src/assets/images/")
+
   return {
     dir: {
       input: "_src",
