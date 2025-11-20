540c229e2064c6379d43923319fc9229af371045
Radu Vlad <radu.a.vlad@gmail.com>
2025-11-20 15:13:06 -0800

    Add a calendly placeholder and work a bit on the process section

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   // Set directories to pass through to the _site folder
-  eleventyConfig.addPassthroughCopy("_src/assets/images/")
+  eleventyConfig.addPassthroughCopy("_src/assets/images/");
 
   return {
     dir: {
