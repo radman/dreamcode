d16b08ac557ac031319f18668db7bc3b9c5b14f4
Radu Vlad <radu.a.vlad@gmail.com>
2025-11-24 00:15:07 -0800

    Work on styling for calendly embed widget

--- _src/_data/site.js
+++ _src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"cta_url": "/#dc-calendly",
   "backgrounds_enabled": false,
-  "use_calendly_placeholder": true,
+  "use_calendly_placeholder": false,
   "env": process.env.ELEVENTY_ENV || "development"
 }
