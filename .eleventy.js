4fee941b4be4dbfdd1ae8d7fee2d4be7ee747b1b
Radu Vlad <radu.a.vlad@gmail.com>
2025-11-25 22:31:27 -0800

    Set year dynamically

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   // Set directories to pass through to the _site folder
   eleventyConfig.addPassthroughCopy("_src/assets/images/");
 
+  // Shortcode for inserting the current year
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
   return {
     dir: {
       input: "_src",
